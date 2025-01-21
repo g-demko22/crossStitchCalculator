adc28437f62af3bd0137834c66f5481d1102b0bf
Garrett Demko <185560604+g-demko22@users.noreply.github.com>
2025-01-21 12:42:05 -0500

    Change spi to fabric count

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const button2 = document.getElementsByClassName('button2')
 const button3 = document.getElementsByClassName('button3')
 const buttons = [button1, button2, button3]
 
-const calculateStitchesBySpi = event => {
+const calculateStitchesByFabricCount = event => {
     const direction = event.srcElement.parentElement.className
-    const spi = document.getElementById(`${direction}-spi`).value
+    const fabricCount = document.getElementById(`${direction}-fabric-count`).value
     const inches = document.getElementById(`${direction}-inches`).value
-    const stiches = spi * inches
+    const stiches = fabricCount / 2 * inches
     document.getElementById(`${direction}-output1`).innerText = `Stitches ${direction} = ${stiches}`
 }
 const calculateDesignRepeats = event => {
@@ -21,13 +21,13 @@ const calculateInchesNeeded = event => {
     const direction = event.srcElement.parentElement.className
     const stitchesInRepeat = document.getElementById(`${direction}-stitches-in-repeat`).value
     const repeats = document.getElementById(`${direction}-repeats`).value
-    const spi2 = document.getElementById(`${direction}-spi2`).value
+    const fabricCount2 = document.getElementById(`${direction}-fabric-count2`).value
     const totalStitches = stitchesInRepeat * repeats
-    const inches = totalStitches / spi2
+    const inches = totalStitches / (fabricCount2 / 2)
     document.getElementById(`${direction}-output3`).innerText = `Inches ${direction} = ${inches.toFixed(2)}`
 }
 
-button1.handler = calculateStitchesBySpi
+button1.handler = calculateStitchesByFabricCount
 button2.handler = calculateDesignRepeats
 button3.handler = calculateInchesNeeded
 
